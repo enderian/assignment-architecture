d4f2fae831125658317e3c68dbb5460123b7019d
MrGoumX <MrGoumX@users.noreply.github.com>
2019-03-26 00:47:53 +0200

    Completed the requested circuit'

--- CPU/CPU.vhd
+++ CPU/CPU.vhd
@@ -1,60 +1,27 @@
-LIBRARY ieee, subcircuits;
+LIBRARY ieee, subcircuits, ALU;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 USE work.subcircuits.all;
-PACKAGE X IS
-	COMPONENT CPU
-		PORT(CTL : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-         A,B: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-         FO: OUT STD_LOGIC_VECTOR (15 DOWNTO 0));
-	END COMPONENT;
-END PACKAGE X;
-LIBRARY ieee, subcircuits;
-USE ieee.std_logic_1164.all;
-USE ieee.std_logic_unsigned.all;
-USE ieee.numeric_std.all;
-USE work.subcircuits.all;
+USE work.ALU.all;
+
 ENTITY CPU IS
-   PORT (CTL : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-			CIN: IN STD_LOGIC;
-         A,B: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-         FO: OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-			COUT: OUT STD_LOGIC);
+   PORT(OP: IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+		CLOCK, CIN: IN STD_LOGIC;
+		IN1, IN2: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+		ALUOUT, FF_1, FF_2, FF_3: OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+		COUT: OUT STD_LOGIC);
 END CPU;
 
 ARCHITECTURE Behavior of CPU IS
-Signal AND_RES: std_logic_vector (15 downto 0);
-Signal OR_RES: std_logic_vector (15 downto 0);
-Signal NOT_RES: std_logic_vector (15 downto 0);
-Signal GEQ_RES: std_logic_vector (15 downto 0);
-Signal ADD_RES: std_logic_vector (15 downto 0);
-Signal SUB_RES: std_logic_vector (15 downto 0);
-Signal ADD_COUT: std_logic;
-Signal ADD_V: std_logic;
-Signal SUB_V: std_logic;
-Signal SUB_COUT: std_logic;
+Signal FF1_OUT: std_logic_vector (15 downto 0);
+Signal FF2_OUT: std_logic_vector (15 downto 0);
+Signal ALU_RES: std_logic_vector (15 downto 0);
+Signal FF3_OUT: std_logic_vector (15 downto 0);
 BEGIN 
-	INST_AND: GATE_AND PORT MAP(A => A, B => B, O => AND_RES);
-	INST_OR: GATE_OR PORT MAP(A => A, B => B, O => OR_RES);
-	INST_NOT: GATE_PR_NOT PORT MAP(A => A, O => NOT_RES);
-	INST_GEQ: GATE_GEQ PORT MAP(A => A, O => GEQ_RES);
-	INST_ADD: FULLADDER_16 PORT MAP(A => A, B => B, CIN => '0', O16 => ADD_RES, V => ADD_V, COUT => ADD_COUT);
-	INST_SUB: SUBTRACTOR_16 PORT MAP(A => A, B => B, CIN => '0', O16 => SUB_RES, V => SUB_V, COUT => SUB_COUT);
-   Process(CTL, A, B)
-   BEGIN
-		case CTL Is
-			when "000" => 
-				FO	<= ADD_RES;
-				COUT <= ADD_COUT;
-			when "001" => 
-				FO <= SUB_RES;
-				COUT <= SUB_COUT;
-			when "010" => FO <= AND_RES;
-			when "011" => FO <= OR_RES;
-			when "100" => FO <= GEQ_RES;
-			when "101" => FO <= NOT_RES;
-			when others => FO <= "0000000000000000";
-       End Case;
-  END PROCESS;
+	FF1: REG PORT MAP(IN1, '1', CLOCK, FF1_OUT);
+	FF2: REG PORT MAP(IN2, '1', CLOCK, FF2_OUT);
+	INST_ALU: ALU16 PORT MAP(OP, CIN, FF1_OUT, FF2_OUT, ALU_RES, COUT);
+	FF3: REG PORT MAP(ALU_RES, '1', CLOCK, FF3_OUT);
+	ALUOUT <= ALU_RES;
 END Behavior;
