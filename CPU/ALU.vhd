663f15931732c3ad8cfda4a45b5cab4dbb48d3f6
MrGoumX <goumaschristos@gmail.com>
2019-03-26 15:56:23 +0200

    Added comments

--- CPU/ALU.vhd
+++ CPU/ALU.vhd
@@ -1,9 +1,15 @@
+--Christos Gkoumas, p3160026
+--Spyridon Pagkalos, p3150133
+
 LIBRARY ieee, subcircuits;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 USE work.subcircuits.all;
+
+--PACKAGE DECLARATION FOR ALU
 PACKAGE ALU IS
+	--COMPONENT DECLARATION FOR ALU16
 	COMPONENT ALU16
 		PORT(CTL : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 			CIN: IN STD_LOGIC;
@@ -12,11 +18,14 @@ PACKAGE ALU IS
 			COUT: OUT STD_LOGIC);
 	END COMPONENT;
 END PACKAGE ALU;
+
 LIBRARY ieee, subcircuits;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 USE work.subcircuits.all;
+
+--ENTITY DECLARATION FOR ALU16
 ENTITY ALU16 IS
    PORT (CTL : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 			CIN: IN STD_LOGIC;
@@ -25,7 +34,9 @@ ENTITY ALU16 IS
 			COUT: OUT STD_LOGIC);
 END ALU16;
 
+--ARCHITECTURE DECLARATION FOR ALU16
 ARCHITECTURE Behavior of ALU16 IS
+--SIGNAL ALL THE TEMP STD_LOGIC & STD_LOGIC_VECTORS
 Signal AND_RES: std_logic_vector (15 downto 0);
 Signal OR_RES: std_logic_vector (15 downto 0);
 Signal NOT_RES: std_logic_vector (15 downto 0);
@@ -37,15 +48,19 @@ Signal ADD_V: std_logic;
 Signal SUB_V: std_logic;
 Signal SUB_COUT: std_logic;
 BEGIN 
+	--INSTANTATE ALL THE APPROPRIATE GATES & FUNCTIONS
 	INST_AND: GATE_AND PORT MAP(A => A, B => B, O => AND_RES);
 	INST_OR: GATE_OR PORT MAP(A => A, B => B, O => OR_RES);
 	INST_NOT: GATE_PR_NOT PORT MAP(A => A, O => NOT_RES);
 	INST_GEQ: GATE_GEQ PORT MAP(A => A, O => GEQ_RES);
 	INST_ADD: FULLADDER_16 PORT MAP(A => A, B => B, CIN => CIN, O16 => ADD_RES, V => ADD_V, COUT => ADD_COUT);
 	INST_SUB: SUBTRACTOR_16 PORT MAP(A => A, B => B, CIN => CIN, O16 => SUB_RES, V => SUB_V, COUT => SUB_COUT);
-   Process(CTL, A, B)
-   BEGIN
+	--CREATE PROCESS FOR CONTROL
+	Process(CTL, A, B)
+	BEGIN
+		--CASES FOR THE CONTROL
 		case CTL Is
+			--FO IS THE FINAL RESULT OF THE ALU, AND COUT IS THE CARRY OUT
 			when "000" => 
 				FO	<= ADD_RES;
 				COUT <= ADD_COUT;
@@ -57,6 +72,6 @@ BEGIN
 			when "100" => FO <= GEQ_RES;
 			when "101" => FO <= NOT_RES;
 			when others => FO <= "0000000000000000";
-       End Case;
-  END PROCESS;
+    	End Case;
+	END PROCESS;
 END Behavior;
--- CPU/CPU.vhd
+++ CPU/CPU.vhd
@@ -1,3 +1,6 @@
+--Christos Gkoumas, p3160026
+--Spyridon Pagkalos, p3150133
+
 LIBRARY ieee, subcircuits, ALU;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
@@ -5,7 +8,9 @@ USE ieee.numeric_std.all;
 USE work.subcircuits.all;
 USE work.ALU.all;
 
+--ENTITY DECLARATION FOR THE FINAL CIRCUIT
 ENTITY CPU IS
+	--PORT MAPPING INs & OUTs
    PORT(OP: IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 		CLOCK, CIN: IN STD_LOGIC;
 		IN1, IN2: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -13,12 +18,16 @@ ENTITY CPU IS
 		COUT: OUT STD_LOGIC);
 END CPU;
 
+--ARCHITECTURE DECLARATION FOR THE FINAL CIRCUIT
 ARCHITECTURE Behavior of CPU IS
+--SIGNAL ALL THE TEMP STD_LOGIC & STD_LOGIC_VECTORS
 Signal FF1_OUT: std_logic_vector (15 downto 0);
 Signal FF2_OUT: std_logic_vector (15 downto 0);
 Signal ALU_RES: std_logic_vector (15 downto 0);
 Signal FF3_OUT: std_logic_vector (15 downto 0);
 BEGIN 
+	--INSTANTATE ALL THE FLIP FLOPS & THE ALU
+	--ALUOUT AND FF1-3 ARE THE RESULTS OF THE ALU
 	FF1: REG PORT MAP(IN1, '1', CLOCK, FF1_OUT);
 	FF2: REG PORT MAP(IN2, '1', CLOCK, FF2_OUT);
 	INST_ALU: ALU16 PORT MAP(OP, CIN, FF1_OUT, FF2_OUT, ALU_RES, COUT);
